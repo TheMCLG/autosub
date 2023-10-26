25d910cb968693f00a8ffc63c15f5f2e734c63ef
TheMCLG <michael@mclg.nl>
2023-10-27 00:09:45 +0200

    Logging fix

--- utils.py
+++ utils.py
@@ -5,9 +5,9 @@ log = logging.getLogger('autosub')
 
 def str_to_bool(s):
     #Converts a string to a boolean value
-    if s == 'True' or 'true':
+    if s == 'True' or s == 'true':
          return True
-    elif s == 'False' or 'false':
+    elif s == 'False' or s == 'false':
          return False
     else:
         log.error(f'Invalid boolean value: {s}')
